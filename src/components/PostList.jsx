70d594234fa7c3b98a1cb10e25751d14d5917720
jhonmike <jhon.msdev@gmail.com>
2024-12-08 11:58:45 -0300

    feat: added fake api and integration posts endpoint

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,36 +1,25 @@
+import { useEffect, useState } from "react"
 import { Avatar, Button, Card } from "antd"
+import { getPosts } from "../services/post"
 import './PostList.css'
 
 const { Meta } = Card
 
-const postsMock = [
-  {
-    id: 1,
-    title: "Primeiro Post",
-    summary: "Este é o resumo do primeiro post",
-    author: "João",
-    date: "01/01/2021"
-  },
-  {
-    id: 2,
-    title: "Segundo Post",
-    summary: "Este é o resumo do segundo post",
-    author: "Maria",
-    date: "02/01/2021"
-  },
-  {
-    id: 3,
-    title: "Terceiro Post",
-    summary: "Este é o resumo do terceiro post",
-    author: "José",
-    date: "03/01/2021"
-  }
-]
-
 const PostList = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts()
+      setPosts(response)
+    }
+
+    fetchPosts()
+  }, [])
+
   return (
     <div className="post-list-container">
-      {postsMock.map((post) => (
+      {posts.map((post) => (
         <Card key={post.id} hoverable style={{ borderRadius: '8px', marginBottom: '20px' }}>
           <Meta
             avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
--- src/components/PostList.test.jsx
+++ src/components/PostList.test.jsx
@@ -1,19 +1,29 @@
-import { describe, expect, it } from "vitest"
-import { render, screen } from "@testing-library/react"
+import { describe, expect, it, vi } from "vitest"
+import { render, screen, waitFor } from "@testing-library/react"
+import * as servicePost from "../services/post"
 import PostList from './PostList'
 
+vi.mock("../services/post")
+
 describe("PostList Component", () => {
-  it("should render the list of posts", () => {
+  it("should render the list of posts", async () => {
+    servicePost.getPosts.mockResolvedValue([
+      { id: 1, title: 'Primeiro Post', summary: 'Este é o resumo do primeiro post', author: 'João', date: '01/01/2021' },
+      { id: 2, title: 'Segundo Post', summary: 'Este é o resumo do segundo post', author: 'Maria', date: '02/01/2021' },
+    ])
+
     render(<PostList />)
 
-    expect(screen.getByText('Primeiro Post')).toBeInTheDocument()
-    expect(screen.getByText('Este é o resumo do primeiro post')).toBeInTheDocument()
-    expect(screen.getByText(/João/)).toBeInTheDocument()
-    expect(screen.getByText(/01\/01\/2021/)).toBeInTheDocument()
+    await waitFor(() => {
+      expect(screen.getByText('Primeiro Post')).toBeInTheDocument()
+      expect(screen.getByText('Este é o resumo do primeiro post')).toBeInTheDocument()
+      expect(screen.getByText(/João/)).toBeInTheDocument()
+      expect(screen.getByText(/01\/01\/2021/)).toBeInTheDocument()
 
-    expect(screen.getByText('Segundo Post')).toBeInTheDocument()
-    expect(screen.getByText('Este é o resumo do segundo post')).toBeInTheDocument()
-    expect(screen.getByText(/Maria/)).toBeInTheDocument()
-    expect(screen.getByText(/02\/01\/2021/)).toBeInTheDocument()
+      expect(screen.getByText('Segundo Post')).toBeInTheDocument()
+      expect(screen.getByText('Este é o resumo do segundo post')).toBeInTheDocument()
+      expect(screen.getByText(/Maria/)).toBeInTheDocument()
+      expect(screen.getByText(/02\/01\/2021/)).toBeInTheDocument()
+    })
   })
 })
